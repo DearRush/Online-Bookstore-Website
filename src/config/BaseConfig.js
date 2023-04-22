96bb5509eb4382d8c4e5ce1b0dd520654c935ad2
杜昊雨 <12832853+kilua_d@user.noreply.gitee.com>
2023-04-22 12:04:58 +0000

    upload
    Signed-off-by: 杜昊雨 <12832853+kilua_d@user.noreply.gitee.com>

---
+++ src/config/BaseConfig.js
@@ -0,0 +1,15 @@
+// 后端API的地址
+const apiURL = "http://localhost:11451";
+export {apiURL};
+
+// 阿里云对象存储 主机地址
+const picHost = "https://ebookpicture.oss-cn-hangzhou.aliyuncs.com";
+export {picHost}
+
+// 阿里云对象存储用户名ID AccessID
+const accessId = "LTAI5tM9SVRoHcGY8meMzB2c";
+export {accessId};
+
+const microServiceURL = "http://localhost:8080/microservice"
+export {microServiceURL};
+
