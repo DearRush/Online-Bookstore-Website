69116cf4d36e12d86aa1e647e3f6f956d5f8c2dc
杜昊雨 <12832853+kilua_d@user.noreply.gitee.com>
2023-05-02 12:29:45 +0000

    删除文件 config-override.js

--- config-override.js
+++
@@ -1,8 +0,0 @@
-/* config-overrides.js */
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    config.externals = {'config': JSON.stringify({
-            apiUrl: 'http://localhost:8080',
-        })};
-    return config;
-};
